2d99bf4a9059354095c42083491fffde9309c85e
Cadman <jcadiente505@gmail.com>
2018-08-10 09:48:12 -0400

    working!

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+var friendArr = require("../data/friends.js");
+
+module.exports = app => {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendArr);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        let newUser = req.body.scores;
+        let scoreTotal = [];
+        let bestMatch = 0;
+
+        for (var i = 0; i < friendArr.length; i++) {
+            var difference = 0;
+            for (var j = 0; j < newUser.length; j++) {
+                difference += (Math.abs(parseInt(friendArr[i].scores[j]) - parseInt(newUser[j])));
+            }
+            scoreTotal.push(difference)
+        }
+
+        for (var i = 0; i < scoreTotal.length; i++) {
+            if(scoreTotal[i] <= scoreTotal[bestMatch]) {
+                bestMatch = i
+            }
+        }
+        let bestFriend = friendArr[bestMatch];
+        
+        res.json(bestFriend)
+        friendArr.push(req.body)
+    });
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Requiring routing pages.
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+//Requiring friend data page.
+var friendArr = require("./app/data/friends.js");
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+});
